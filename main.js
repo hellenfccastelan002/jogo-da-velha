7309508bfa278db83a9d0b9452570833be2e88db
HELLEN <hellen.castelan@escola.pr.gov.br>
2025-12-01 09:22:19 -0300

    .

---
+++ main.js
@@ -0,0 +1,17 @@
+//variáveis
+    let gameState = ['', '', '', '', '', '', '', '', ''];
+    let activePlayer = 'X';
+    let isGameRunning = true;
+
+    //constante para condições de vitória
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    
